e7fd753f051525ac4c37426dc1ce9cdf7ee5e8b8
PedroPertusi <pedrovazpertusi@gmail.com>
2022-09-09 16:56:21 -0300

    Implementando a funcao barrelShifter

--- hw/components.py
+++ hw/components.py
@@ -9,7 +9,6 @@ def and16(a, b, q):
     a: 16 bits
     b: 16 bits
     q: 16 bits
-
     and bit a bit entre a e b
     """
     foo = Signal(0)
@@ -25,7 +24,6 @@ def and16(a, b, q):
 def or8way(a, b, c, d, e, f, g, h, q):
     """
     a, b, c, ... h: 1 bit
-
     or bit a bit entre a e b
     """
     foo = Signal(intbv(0))
@@ -42,7 +40,6 @@ def orNway(a, q):
     """
     a: 16 bits
     q: 1 bit
-
     or bit a bit dos valores de a: a[0] or a[1] ...
     """
     foo = Signal(intbv(0))
@@ -52,7 +49,6 @@ def orNway(a, q):
         q.next = foo
 
     return comb
-
 
 @block
 def barrelShifter(a, dir, size, q):
@@ -68,12 +64,9 @@ def barrelShifter(a, dir, size, q):
     exemplo: a = 0000 1111 0101 1010, dir = 0, size = 3
              q = 0111 1010 1101 0000
     """
-    foo = Signal(intbv(0))
-
     @always_comb
     def comb():
-        q.next = foo
-
+        q.next = a >> size if not dir else a << size #ideia retirada de https://stackoverflow.com/questions/22832615/what-do-and-mean-in-python
     return comb
 
 
@@ -84,7 +77,6 @@ def mux2way(q, a, b, sel):
     a: 16 bits
     b: 16 bits
     sel: 2 bits
-
     Mux entre a e b, sel é o seletor
     """
     foo = Signal(intbv(0))
@@ -105,7 +97,6 @@ def mux4way(q, a, b, c, d, sel):
     c: 16 bits
     d: 16 bits
     sel: 4 bits
-
     Mux entre a, b, c, d sel é o seletor
     """
     foo = Signal(intbv(0))
@@ -136,11 +127,8 @@ def mux8way(q, a, b, c, d, e, f, g, h, sel):
 def deMux2way(a, q0, q1, sel):
     """
     deMux de 2 saídas e uma entrada.
-
     - Lembre que a saída que não está ativada é 0
-
     Exemplo:
-
     a = 0xFFAA, sel = 0
     q0 = 0xFFAA
     q1 = 0
@@ -159,7 +147,6 @@ def deMux2way(a, q0, q1, sel):
 def deMux4way(a, q0, q1, q2, q3, sel):
     """
     deMux de 4 saídas e uma entrada.
-
     - Lembre que a saída que não está ativada é 0
     """
 
@@ -176,7 +163,6 @@ def deMux4way(a, q0, q1, q2, q3, sel):
 def deMux8way(a, q0, q1, q2, q3, q4, q5, q6, q7, sel):
     """
     deMux de 8 saídas e uma entrada.
-
     - Lembre que a saída que não está ativada é 0
     """
 
@@ -211,7 +197,6 @@ def bin2bcd(b, bcd1, bcd0):
     """
     componente que converte um vetor de b[8:] (bin)
     para dois digitos em BCD
-
     Exemplo:
     bin  = `01010010`
     BCD1 = 8
