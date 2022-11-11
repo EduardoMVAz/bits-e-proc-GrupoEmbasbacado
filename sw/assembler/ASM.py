02a2ea6eb3309da2ebf15ccde8546da40c9492ec
PedroPertusi <pedrovazpertusi@gmail.com>
2022-11-11 18:39:46 -0300

    Implementando o Leaw em D (A)

--- sw/assembler/ASM.py
+++ sw/assembler/ASM.py
@@ -35,14 +35,18 @@ class ASM:
         self.parser.reset()
         self.hackLineCount = 0
         numb_of_jumps_no_nop = 0
+        num_of_leaw_d = 0
         last_command = ['a']
         while self.parser.advanced():
             if last_command[0][0] == 'j' and self.parser.currentCommand[0] != 'nop':
                 numb_of_jumps_no_nop += 1
+            
+            if self.parser.currentCommand[0] == 'leaw' and self.parser.currentCommand[2] == '%D':
+                num_of_leaw_d += 1
 
             if self.parser.commandType() == 'L_COMMAND':
                 if not self.symbolTable.contains(self.parser.label()):
-                    self.symbolTable.addEntry(self.parser.label(), self.hackLineCount + numb_of_jumps_no_nop)
+                    self.symbolTable.addEntry(self.parser.label(), self.hackLineCount + numb_of_jumps_no_nop + num_of_leaw_d)
             else:
                 self.hackLineCount += 1
             last_command = self.parser.currentCommand
@@ -64,6 +68,7 @@ class ASM:
         while self.parser.advanced():
             cmnd = self.parser.currentCommand[0]            
 
+
             if self.parser.commandType() == "A_COMMAND":
                 symbol = self.parser.symbol()
                 try:
@@ -73,6 +78,8 @@ class ASM:
                 bin = '00' + self.code.toBinary(symbol)
                 string = str(bin + "\n")
                 allStrings += string
+                if self.parser.currentCommand[2] == '%D':
+                        allStrings += '100001100000010000\n'
             elif self.parser.commandType() == "L_COMMAND":
                 pass
 
